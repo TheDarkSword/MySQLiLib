5d9b825ad6657121dc383ff6458d032b55480f88
TheDarkSword <TheDarkSword01@gmail.com>
2019-02-26 00:14:09 +0100

    Example

--- index.php
+++ index.php
@@ -19,6 +19,19 @@
             "username VARCHAR(100) NOT NULL",
             "password VARCHAR(100) NOT NULL"
         ));
+
+        //$mysql->removeLine("username", "TheDarkSword01");
+        if($mysql->lineExists("username", "TheDarkSword01")){
+            echo "<br>Line exist";
+            echo "<br>";
+            $result = $mysql->getValue("username", "TheDarkSword01", "password");
+            echo $result;
+        } else {
+            echo "<br>Line not exist";
+            $mysql->addLineArray(array("username", "password"), array("TheDarkSword01", "ciao1234"));
+        }
+
+        $mysql->close();
     ?>
     </body>
 </html>
